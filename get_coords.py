4c99b6ee1c50ef85e8fe24e9edca181eefe23e5d
skunz42 <skunz598@gmail.com>
2021-06-13 14:43:35 -0400

    added coords

--- get_coords.py
+++ get_coords.py
@@ -2,16 +2,36 @@ import json
 import urllib
 import urllib.request
 import sys
+    
+ESRI_KEY = ""
+KM = 0.015
 
-def calc_coords(key, city, state, coords):
+def get_resp(city, state):
     url = ('https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates'
         '?singleLine=%s,%s'
         '&f=%s'
-        '&token=%s') % (city, state, 'pjson', key)
+        '&token=%s') % (city, state, 'pjson', ESRI_KEY)
     response = urllib.request.urlopen(url)
     jsonRaw = response.read()
     jsonData = json.loads(jsonRaw)
-    print(jsonData)
+    return jsonData
+
+def calc_coords(json_resp, coords):
+    lngsw = json_resp["candidates"][0]["extent"]["xmin"]
+    latsw = json_resp["candidates"][0]["extent"]["ymin"]
+    lngne = json_resp["candidates"][0]["extent"]["xmax"]
+    latne = json_resp["candidates"][0]["extent"]["ymax"]
+
+    templng = lngsw
+    templat = latsw
+
+    while templat <= latne:
+        while templng <= lngne:
+            coords.append((templat, templng))
+            templng += KM
+        templng = lngsw
+        templat += KM
+
 
 def main():
     if len(sys.argv) != 3:
@@ -20,10 +40,12 @@ def main():
 
     city = str(sys.argv[1])
     state = str(sys.argv[2])
-
     ESRI_KEY = open("esrikey.txt", "r").read()
+
     coords = []
 
-    calc_coords(ESRI_KEY, city, state, coords)
+    json_resp = get_resp(city, state)
+    calc_coords(json_resp, coords)
+    print(coords)
 
 main()
